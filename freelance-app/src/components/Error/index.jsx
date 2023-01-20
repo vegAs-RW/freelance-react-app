28315c8c0054de96af4f53bf37d41d6c43df0acb
Jordan <jordan.pouchot2@gmail.com>
2023-01-20 16:15:14 +0100

    update route

---
+++ freelance-app/src/components/Error/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Error = () => {
+    return (
+        <div>
+            <h1>Cette page n'existe pas !</h1>
+        </div>
+    );
+};
+
+export default Error;
--- freelance-app/src/components/Header/index.jsx
+++ freelance-app/src/components/Header/index.jsx
@@ -5,7 +5,8 @@ const Header = () => {
     return (
         <nav>
             <Link to='/'>Acceuil</Link>
-            <Link to="/survey/42">Questionnaire</Link>
+            <Link to="/survey/1">Questionnaire</Link>
+            <Link to="/freelances">Profils</Link>
         </nav>
     );
 };
