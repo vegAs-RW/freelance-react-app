32170dbb46c2f7100dad8f3b3cb1cbf8edc91049
Jordan <jordan.pouchot2@gmail.com>
2023-01-24 11:08:11 +0100

    add stylised component

--- freelance-app/src/pages/Freelances/index.jsx
+++ freelance-app/src/pages/Freelances/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import styled from 'styled-components';
 import DefaultPicture from '../../assets/profile.png'
 import Card from '../../components/Card';
+import colors from '../../utils/style/colors';
 
 
 const freelanceProfiles = [
@@ -19,19 +21,51 @@ const freelanceProfiles = [
         jobTitle: 'Développeuse Fullstack',
         picture: DefaultPicture,
     },
+    {
+        name: 'Kevin Smith',
+        jobTitle: 'Développeur Front',
+        picture: DefaultPicture,
+    },
 ]
+
+const CardsContainer= styled.div`
+        display: grid;
+        gap: 24px;
+        grid-template-rows:350px 350px;
+        grid-template-columns:repeat(2, 1fr);
+        align-items: center;
+        justify-items: center;
+`
+
+const PageTitle = styled.h1 `
+    font-size: 30px;
+    color: black;
+    text-align: center;
+    padding-bottom: 30px;
+`
+
+const PageSubtitle = styled.h2`
+        font-size: 20px;
+        color: ${colors.secondary};
+        font-weight: 300;
+        text-align: center;
+        padding-bottom: 30px;
+`
 
 const Freelances = () => {
    
     return (
         <div>
-            <h1>Freelances</h1>
+            <PageTitle>Trouvez votre freelance</PageTitle>
+            <PageSubtitle>Chez Shinyy vous trouverez le profil qui correspond a votre recherche</PageSubtitle>
+            <CardsContainer>
             {freelanceProfiles.map((profil, index) => (
                 <Card key={`${profil.name}-${index}`}
                         label={profil.jobTitle}   
                         title={profil.name}
                         />
             ))}
+            </CardsContainer>
         </div>
     );
 };
