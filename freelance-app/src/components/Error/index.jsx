5850e419cb9cf24800df86c1bd1aa6ea60002225
Jordan <jordan.pouchot2@gmail.com>
2023-01-24 10:37:14 +0100

    add stylised components to Error component

--- freelance-app/src/components/Error/index.jsx
+++ freelance-app/src/components/Error/index.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+import colors from '../../utils/style/colors';
 
+const ErrorWrapper = styled.div`
+        margin: 30px;   
+        display: flex;
+        flex-direction: column;
+        background-color: ${colors.backgroundLight};
+        align-items: center;
+`
+
+const ErrorTitle = styled.h1`
+    font-weight: 300;
+`
+
+const ErrorSubtitle = styled.h2`
+        font-weight: 300;
+        color: ${colors.secondary};
+`
+const Illustration = styled.img`
+    max-width: 800px;
+`
 
 const Error = () => {
     return (
-        <div>
-            <h1>Cette page n'existe pas !</h1>
-        </div>
+        <ErrorWrapper>
+            <ErrorTitle>Ooups ...</ErrorTitle>
+            <Illustration src='https://github.com/atoulmet/assets/blob/master/404.svg?raw=true' />
+            <ErrorSubtitle>
+                La page que vous recherchez n'existe pas
+            </ErrorSubtitle>
+        </ErrorWrapper>
     );
 };
 
