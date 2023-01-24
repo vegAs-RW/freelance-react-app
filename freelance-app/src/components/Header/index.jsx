1c5671cde59f5a606c3f72d9eee79f3632d93b18
Jordan <jordan.pouchot2@gmail.com>
2023-01-24 11:07:26 +0100

    add stylised component

--- freelance-app/src/components/Header/index.jsx
+++ freelance-app/src/components/Header/index.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import styled from 'styled-components';
+import colors from '../../utils/style/colors';
+import DarkLogo from '../../assets/dark-logo.png';
+
+const HomeLogo = styled.img`
+        height: 70px;
+`
+
+const StyledLink = styled(Link)`
+        padding: 15px;
+        color: #8186a0;
+        text-decoration: none;
+        font-size: 18px;
+        ${(props) => 
+        props.$isFullLink &&
+        `color: white; border-radius:30px; background-color: ${colors.primary}`}
+
+`
+const NavContainer = styled.nav`
+        padding: 30px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+`
 
 const Header = () => {
     return (
-        <nav>
-            <Link to='/'>Acceuil</Link>
-            <Link to="/survey/1">Questionnaire</Link>
-            <Link to="/freelances">Profils</Link>
-        </nav>
+        <NavContainer>
+            <Link to="/"><HomeLogo src={DarkLogo}/></Link>
+            <StyledLink to='/'>Acceuil</StyledLink>
+            <StyledLink to="/survey/1" $isFullLink>Questionnaire</StyledLink>
+            <StyledLink to="/freelances">Profils</StyledLink>
+        </NavContainer>
     );
 };
 
